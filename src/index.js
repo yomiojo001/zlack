24594966796e6ddc247397ba88e80c992a5fc0b1
Micheal Ojo <yomiojo001@gmail.com>
2020-10-07 14:36:26 +0100

    cleanup

--- src/index.js
+++ src/index.js
@@ -66,15 +66,12 @@ io.on('connection', (socket) => {
 })
 
 
-// Serve static assets if in production
-if(process.env.NODE_ENV === 'production'){
-    // Set static folder
-    app.use(express.static('../client/build'));
+// Set static folder
+app.use(express.static(path.join(__dirname, '../client/build')));
 
-    app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
-    })
-}
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 
 // server listens on port
 server.listen(port, () => {
